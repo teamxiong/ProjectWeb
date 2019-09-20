f354be6651a15d78979502e8d9ef423ccdeaff8b
team_xiong@outlook.com <team_xiong@outlook.com>
2019-09-20 16:10:18 +0800

    提交

--- ProjectWeb/Startup.cs
+++ ProjectWeb/Startup.cs
@@ -26,12 +26,12 @@ namespace ProjectWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
 
             services.AddSession(opts=> {
                 int timespan = 20;
